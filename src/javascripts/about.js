042cfc23f04882849ed8b444e27d19279c001e56
wahn1 <143635058+wahn1@users.noreply.github.com>
2023-11-02 17:08:06 -0400

    added stuff

--- src/javascripts/about.js
+++ src/javascripts/about.js
@@ -1,1 +1,7 @@
-console.log('Hello from about.js!');
+var programConfirmation = prompt("What's Will's favorite Honors Program? (You will need to answer this right to login!)");
+
+if (programConfirmation === "QUEST") {
+  alert("Yay!");
+} else {
+  alert("Damn... Good Luck Next Time!");
+}
