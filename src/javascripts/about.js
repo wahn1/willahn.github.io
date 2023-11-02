b8b218d060dee0a8b19882aa0a1c65243856d2a2
wahn1 <143635058+wahn1@users.noreply.github.com>
2023-11-02 17:14:52 -0400

    changed about js

--- src/javascripts/about.js
+++
@@ -1,7 +0,0 @@
-var programConfirmation = prompt("What's Will's favorite Honors Program? (You will need to answer this right to login!)");
-
-if (programConfirmation === "QUEST") {
-  alert("Yay!");
-} else {
-  alert("Damn... Good Luck Next Time!");
-}
